7837eafe5899bf14524e81dffc0691f73945143a
AmandaLeopoldoo <amandaleopoldo2930@gmail.com>
2025-08-24 23:23:51 -0300

    Feat: Criando as entidades do projeto

--- src/main/java/com/amandaleopoldoo/postocombustivel/infraestructure/entities/Abastecimento.java
+++ src/main/java/com/amandaleopoldoo/postocombustivel/infraestructure/entities/Abastecimento.java
@@ -1,4 +1,38 @@
 package com.amandaleopoldoo.postocombustivel.infraestructure.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "abastecimentos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Abastecimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @OneToOne // Um abastecimento para uma bomba (no momento do registro)
+    @JoinColumn(name = "bomba_id")
+    private BombaDeCombustivel bombaDeCombustivel;
+
+    @Column(name = "data_abastecimento")
+    private LocalDate dataDoAbastecimento;
+
+    @Column(name = "quantidade_litros")
+    private Long quantidadeDelitros;
+
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+
+
+
 }
--- src/main/java/com/amandaleopoldoo/postocombustivel/infraestructure/entities/BombaDeCombustivel.java
+++ src/main/java/com/amandaleopoldoo/postocombustivel/infraestructure/entities/BombaDeCombustivel.java
@@ -1,4 +1,26 @@
 package com.amandaleopoldoo.postocombustivel.infraestructure.entities;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bombas_de_Combustivel")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BombaDeCombustivel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @ManyToOne // Muitas bombas podem ter um tipo de combustível (ex: várias bombas de gasolina)
+    @JoinColumn(name = "combustivel_id")
+    private TipoDeCombustivel tipoDeCombustivel;
 }
